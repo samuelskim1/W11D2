1a4285cdd43e0c55c28355330c51caebae19db04
Samuel Kim <samuelskim420@gmail.com>
2023-03-21 19:03:04 +0000

    finished phase 1

---
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Clock, { ClockToggle } from './components/Clock';
+import Folder from './components/Folder';
+import Weather from './components/Weather';
+import Autocomplete from './components/Autocomplete';
+import { useState } from 'react';
+
+const names = [
+  'Abba',
+  'Barbara',
+  'Barney',
+  'Jeff',
+  'Jenny',
+  'Sally',
+  'Sarah',
+  'Xander'
+];
+
+const folders = [
+  { title: 'one', content: 'I am the first' },
+  { title: 'two', content: 'Second folder here' },
+  { title: 'three', content: 'Third folder here' }
+];
+
+function App () {
+  const [showClock, setShowClock] = useState(true);
+  
+  const toggleClock = () => setShowClock((oldShowClock) => (!oldShowClock));
+  
+  return (
+    <div className="widgets">
+      <Folder folders={folders} />
+      <Weather />
+      <ClockToggle toggleClock={toggleClock} />
+      {showClock && <Clock />}
+      <Autocomplete names={names} />
+    </div>
+  );
+
+}
+
+export default App;
